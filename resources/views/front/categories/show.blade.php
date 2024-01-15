73ec4aef643d116aec1a1cc1ea191f5a5a99971b
Muhammad Yahya <muhammad.yahya.asaan@gmail.com>
2024-01-15 16:36:19 +0500

    Category fix

--- resources/views/front/categories/show.blade.php
+++ resources/views/front/categories/show.blade.php
@@ -25,41 +25,41 @@
                     </div> --}}
                     @foreach ($posts as $post)
 
-                    @endforeach
-                    <article class="brick entry" data-aos="fade-up">
+                    
+                        <article class="brick entry" data-aos="fade-up">
 
-                        <div class="entry__thumb">
-                            <a href="{{ route('posts.view', $post->id) }}" class="thumb-link">
-                                <img src="{{ asset('images/' . $post->image) }}" alt="">
-                            </a>
-                        </div> <!-- end entry__thumb -->
+                            <div class="entry__thumb">
+                                <a href="{{ route('posts.view', $post->id) }}" class="thumb-link">
+                                    <img src="{{ asset('images/' . $post->image) }}" alt="">
+                                </a>
+                            </div> <!-- end entry__thumb -->
 
-                        <div class="entry__text">
-                            <div class="entry__header">
-                                <h1 class="entry__title"><a
-                                        href="{{ route('posts.view', $post->id) }}">{{ $post->title }}</a></h1>
+                            <div class="entry__text">
+                                <div class="entry__header">
+                                    <h1 class="entry__title"><a
+                                            href="{{ route('posts.view', $post->id) }}">{{ $post->title }}</a></h1>
 
-                                <div class="entry__meta">
-                                    <span class="byline">By:
-                                        <span class='author'>
-                                            <a href="#">{{ $post->user->name }}</a>
+                                    <div class="entry__meta">
+                                        <span class="byline">By:
+                                            <span class='author'>
+                                                <a href="#">{{ $post->user->name }}</a>
+                                            </span>
                                         </span>
-                                    </span>
-                                    <span class="cat-links">
-                                        <a href="#">{{ $category->title }}</a>
-                                    </span>
+                                        <span class="cat-links">
+                                            <a href="#">{{ $category->title }}</a>
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="entry__excerpt">
-                                <p>
-                                    {{ $post->excerpt }}
-                                </p>
-                            </div>
-                            <a class="entry__more-link" href="{{ route('posts.view', $post->id) }}">Learn More</a>
-                        </div> <!-- end entry__text -->
+                                <div class="entry__excerpt">
+                                    <p>
+                                        {{ $post->excerpt }}
+                                    </p>
+                                </div>
+                                <a class="entry__more-link" href="{{ route('posts.view', $post->id) }}">Learn More</a>
+                            </div> <!-- end entry__text -->
 
-                    </article> <!-- end article -->
-
+                        </article> <!-- end article -->
+                    @endforeach
                 </div> <!-- end brick-wrapper -->
 
             </div> <!-- end masonry -->
